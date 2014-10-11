8020e2bc44724ea55928f56696675791d4d6d42b
Christian Rubiales <christianrubiales@yahoo.com>
2014-10-11 15:13:41 +0800

    Update PassByValueStackOverflow.java

--- PassByValueStackOverflow.java
+++ PassByValueStackOverflow.java
@@ -5,39 +5,39 @@ import java.lang.*;
 import java.io.*;
 
 /* http://stackoverflow.com/a/12429953 */
-class PassByValueStackOverflow
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
+class PassByValueStackOverflow {
+	public static void main(String[] args) throws java.lang.Exception {
 		Foo f = new Foo("f");
-		
-        changeReference(f); // It won't change the reference!
-        System.out.println(f.getAttribute());
-        
-        modifyReference(f); // It will modify the object that the reference variable "f" refers to!
-        System.out.println(f.getAttribute());
+
+		changeReference(f); // It won't change the reference!
+		System.out.println(f.getAttribute());
+
+		modifyReference(f); // It will modify the object that the reference
+							// variable "f" refers to!
+		System.out.println(f.getAttribute());
 	}
-	     
-	public static void changeReference(Foo a){
-          Foo b = new Foo("b");
-          a = b;
-     }
-     public static void modifyReference(Foo c){
-          c.setAttribute("c");
-     }
+
+	public static void changeReference(Foo a) {
+		Foo b = new Foo("b");
+		a = b;
+	}
+
+	public static void modifyReference(Foo c) {
+		c.setAttribute("c");
+	}
 }
 
 class Foo {
 	private String attribute;
-	
+
 	public Foo(String attribute) {
 		this.attribute = attribute;
 	}
-	
+
 	public void setAttribute(String attribute) {
 		this.attribute = attribute;
 	}
-	
+
 	public String getAttribute() {
 		return this.attribute;
 	}
