14f3af1a04e62c55b257700e9a519cd637df0b6a
Christian Rubiales <christianrubiales@yahoo.com>
2014-10-11 16:35:44 +0800

    Created GcdByTrialDivision.java

---
+++ math/GcdByTrialDivision.java
@@ -0,0 +1,31 @@
+package math;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GcdByTrialDivision
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		System.out.println(gcd(90000000, 100000000));
+	}
+	
+	/**
+	 * Start with the lower of the two given numbers,
+	 * then start trying to divide both given numbers with the smaller number.
+	 * If they can be both divided by the number, then that is the GCD.
+	 * Else, keep decrementing the test number by 1 until 1 is reached 
+	 * or the test number can now divide both numbers. 
+	 */
+	public static int gcd(int a, int b) {
+		for (int i = Math.min(a, b); i >= 1; i--) {
+			if (a%i==0 && b%i==0) {
+				return i;
+			}
+		}
+		
+		return 1;
+	}
+}
