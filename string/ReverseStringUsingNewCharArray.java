bf572f6963a1ef35b01348872857fbff77151813
Christian Rubiales <christianrubiales@yahoo.com>
2014-10-11 16:11:45 +0800

    Create ReverseStringUsingNewCharArray.java

---
+++ string/ReverseStringUsingNewCharArray.java
@@ -0,0 +1,27 @@
+package string;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ReverseStringUsingNewCharArray
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println(new String(reverseString("12345".toCharArray())));
+	}
+	
+	/**
+	 * Copy each character from the source string to the new string in reverse order.
+	 */
+	public static char[] reverseString(char[] string) {
+		char[] reverse = new char[string.length];
+		
+		int x = string.length;
+		for (int i = 0; i < x; i++) {
+			reverse[i] = string[x-i-1];
+		}
+		
+		return reverse;
+	}
+}
