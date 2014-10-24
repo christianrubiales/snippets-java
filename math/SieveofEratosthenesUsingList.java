5ce59b6d1f87baf7ea5ded4657b0f151237c387b
Christian Rubiales <christianrubiales@yahoo.com>
2014-10-24 18:39:01 +0800

    Create SieveofEratosthenesUsingList.java

---
+++ math/SieveofEratosthenesUsingList.java
@@ -0,0 +1,34 @@
+package math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SieveofEratosthenesUsingList {
+
+	public static void main(String[] args) {
+		System.out.println(getPrimes(30)); // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+		System.out.println(getPrimes(121)); // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113
+	}
+
+	public static List<Integer> getPrimes(int max) {
+		List<Integer> primes = new ArrayList<Integer>();
+		
+		// populate with all number between 2 to max
+		for (int i = 2; i <= max; i++) {
+			primes.add(i);
+		}
+		
+		for (int i = 2; i <= Math.sqrt(max); i++) {
+			// remove the succeeding multiples of a base number
+			for (int j = i; j <= max;) {
+				j += i;
+				if (primes.contains(j)) {
+					primes.remove((Integer) j);
+				}
+			}
+		}
+		
+		return primes;
+	}
+	
+}
