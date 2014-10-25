60793a7dac6f6dc2e86f9337328ce5becb983923
Christian Rubiales <christianrubiales@yahoo.com>
2014-10-25 23:55:15 +0800

    ValidParenthesesSingleCharacterTypeUsingArray

--- parentheses/ValidParenthesesSingleCharacterTypeUsingArray.java
+++ parentheses/ValidParenthesesSingleCharacterTypeUsingArray.java
@@ -7,6 +7,7 @@ package parentheses;
 public class ValidParenthesesSingleCharacterTypeUsingArray {
 
 	public static void main(String[] args) {
+		System.out.println("true = " + isValid("")); // true
 		System.out.println("true = " + isValid("()")); // true
 		System.out.println("true = " + isValid("()()()")); // true
 		System.out.println("false = " + isValid("((")); // false
