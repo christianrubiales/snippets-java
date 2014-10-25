a6799682ca60d4a16c4483f9f6cb2df77ab72c45
Christian Rubiales <christianrubiales@yahoo.com>
2014-10-25 23:54:54 +0800

    ValidParenthesesMultipleCharacterTypesUsingStack

--- parentheses/ValidParenthesesMultipleCharacterTypesUsingStack.java
+++ parentheses/ValidParenthesesMultipleCharacterTypesUsingStack.java
@@ -9,6 +9,7 @@ import java.util.Stack;
 public class ValidParenthesesMultipleCharacterTypesUsingStack {
 
 	public static void main(String[] args) {
+		System.out.println("true = " + isValid("")); // true
 		System.out.println("true = " + isValid("()")); // true
 		System.out.println("true = " + isValid("()()()")); // true
 		System.out.println("false = " + isValid("((")); // false
