2b745ad87b947562def279150070e8134b850085
Christian Rubiales <christianrubiales@yahoo.com>
2015-01-09 21:12:23 +0800

    Modulo

---
+++ math/Modulo.java
@@ -0,0 +1,43 @@
+package math;
+
+/**
+ * Remainder after division
+ */
+public class Modulo {
+
+	public static void main(String[] args) {
+//		System.out.println(4 % 1);//division by 0 error
+		System.out.println(4 % 1);
+		System.out.println(4 % 2);
+		System.out.println(4 % 3);
+		System.out.println(4 % 4);
+		System.out.println(4 % 5);
+		System.out.println(0 % 4);
+		System.out.println(1 % 4);
+		System.out.println(2 % 4);
+		System.out.println(3 % 4);
+		System.out.println(4 % 4);
+		System.out.println(5 % 4);
+
+		System.out.println();
+//		System.out.println(mod(4, 0));//division by 0 error
+		System.out.println(mod(4, 1));//0
+		System.out.println(mod(4, 2));//0
+		System.out.println(mod(4, 3));//1
+		System.out.println(mod(4, 4));//0
+		System.out.println(mod(4, 5));//4
+		System.out.println(mod(0, 4));//0
+		System.out.println(mod(1, 4));//1
+		System.out.println(mod(2, 4));//2
+		System.out.println(mod(3, 4));//3
+		System.out.println(mod(4, 4));//0
+		System.out.println(mod(5, 4));//1
+	}
+	
+	/**
+	 * From wikipedia: gcd(a, b) = a - b(floor(a/b))
+	 */
+	public static int mod(int a, int b) {
+		return a - b * (a/b);
+	}
+}
