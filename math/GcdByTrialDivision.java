b8697a799359c7893b522cf33cf96179fe08e71b
Christian Rubiales <christianrubiales@yahoo.com>
2015-01-09 20:50:22 +0800

    GcdByTrialDivision

--- math/GcdByTrialDivision.java
+++ math/GcdByTrialDivision.java
@@ -17,7 +17,7 @@ class GcdByTrialDivision
 	 */
 	public static int gcd(int a, int b) {
 		for (int i = Math.min(a, b); i >= 1; i--) {
-			if (a%i==0 && b%i==0) {
+			if (a % i == 0 && b % i == 0) {
 				return i;
 			}
 		}
