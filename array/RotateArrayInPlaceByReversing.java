51fd83d5fc1f1408dc4d764ccd9a920a1a36c9f6
Christian Rubiales <christianrubiales@yahoo.com>
2015-01-11 22:44:36 +0800

    RotateArrayInPlaceByReversing

---
+++ array/RotateArrayInPlaceByReversing.java
@@ -0,0 +1,43 @@
+package array;
+
+/**
+ * Tricky when it comes to indices
+ * @see http://leetcode.com/2010/04/rotating-array-in-place.html
+ */
+public class RotateArrayInPlaceByReversing {
+
+	public static void main(String[] args) {
+		String s = "123456789";
+		for (int i = 0; i < s.length() + 10; i++) {
+			System.out.println(rotateRight(s.toCharArray(), i));
+		}
+	}
+	
+	/**
+	 * 1) Reverse the whole array
+	 * 2) Reverse the first (length - places)
+	 * 3) Reverse the last places, if places > 0
+	 */
+	public static char[] rotateRight(char[] array, int places) {
+		reverse(array, 0, array.length - 1);
+		reverse(array, 0, array.length - 1 - places);
+		if (places > 0) {
+			reverse(array, array.length - places, array.length - 1);
+		}
+		
+		return array;
+	}
+	
+	public static char[] reverse(char[] array, int startIndex, int endIndex) {
+		char temp;
+		int length = endIndex - startIndex;
+		for (int i = 0; i <= length / 2; i++) {
+			// just a normal swap
+			temp = array[endIndex - i];
+			array[endIndex - i] = array[startIndex + i];
+			array[startIndex + i] = temp;
+		}
+		return array;
+	}
+
+}
